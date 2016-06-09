5d0ffb8dcd9a957b7b03f38da5457cc59233d7e8
Andrew Carnes <carnesandnoble@gmail.com>
2016-06-09 13:40:15 +0200

    Added iml study.

---
+++ studies/iml/bin/BasicTrainAndTest.cxx
@@ -0,0 +1,242 @@
+//////////////////////////////////////////////////////////////////////////
+//                            BasicTrainAndTest.cxx                     //
+// =====================================================================//
+//                                                                      //
+//   Train the forest, save the trees, and test the results.            //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////
+// _______________________Includes_______________________________________//
+///////////////////////////////////////////////////////////////////////////
+
+#include "Forest.h"
+#include "Functions.h"
+#include "Utilities.h"
+#include "LoadSaveEvents.h"
+
+#include "TRandom3.h"
+#include "TStopwatch.h"
+#include "TROOT.h"
+#include "TTree.h"
+#include "TNtuple.h"
+#include "TFile.h"
+#include "TH1D.h"
+#include "TGraph.h"
+#include "TCanvas.h"
+#include "TChain.h"
+#include "TMatrixD.h"
+#include "TVectorD.h"
+#include "TXMLEngine.h"
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <fstream>
+#include <utility>
+
+//////////////////////////////////////////////////////////////////////////
+// ______________________Regression_Settings ___________________________//
+/////////////////////////////////////////////////////////////////////////
+
+// Set the settings for the regression here. You may choose to overwrite these values in
+// main if you want input from the terminal to determine the settings.
+
+// Fundamental settings for the regression.
+Int_t nodes = 16;
+Int_t trees = 64;
+Double_t lr = 0.3;
+
+// Choose which loss function to use.
+//LossFunction* lf = new LeastSquares();
+//LossFunction* lf = new AbsoluteDeviation();
+LossFunction* lf = new Huber();                 // Default Huber, tails are the largest 30% of the residuals
+//LossFunction* lf = new Huber(0.5);            // Huber considering the largest 50% of the residuals to be outliers
+
+// Choose which transform to use.
+TransformFunction* transform = 0;
+                 //transform = new Inverse();
+                 //transform = new Log();
+
+// Whether to save the trees from the regression into a directory specified later.
+bool saveTrees = true;
+
+// Where to save the trees.
+TString treesDirectory("../trees/");
+
+/////////////////////////////////////////////////////////////////////////
+// ---------------------------------------------------------------------
+/////////////////////////////////////////////////////////////////////////
+
+void saveSettingsToXML(const char* directory)
+{
+    std::stringstream wvars;
+
+    const char* none = "NONE";
+    std::stringstream filename;
+    filename << directory << "settings.xml";
+
+    TXMLEngine* xml = new TXMLEngine();
+    XMLNodePointer_t root = xml->NewChild(0,0,"root");
+    XMLNodePointer_t settings = xml->NewChild(root,0,"settings");
+    xml->NewAttr(settings, 0, "nodes", numToStr(nodes).c_str());
+    xml->NewAttr(settings, 0, "trees", numToStr(trees).c_str());
+    xml->NewAttr(settings, 0, "learning_rate", numToStr(lr).c_str());
+    xml->NewAttr(settings, 0, "loss_function", lf->name().c_str());
+    xml->NewAttr(settings, 0, "transform", ((transform!=0)?transform->name():none));
+
+    XMLDocPointer_t xmldoc = xml->NewDoc();
+    xml->DocSetRootElement(xmldoc, root);
+
+    xml->SaveDoc(xmldoc, filename.str().c_str());
+    xml->FreeDoc(xmldoc);
+    delete xml;
+}
+
+
+/////////////////////////////////////////////////////////////////////////
+// ---------------------------------------------------------------------
+/////////////////////////////////////////////////////////////////////////
+
+void displaySettingsFromXML(const char* directory)
+{
+    TXMLEngine* xml = new TXMLEngine;
+    XMLDocPointer_t xmldoc = xml->ParseFile(TString(directory)+"settings.xml");
+    if(xmldoc==0)
+    {
+        delete xml;
+        return;
+    }
+    XMLNodePointer_t root = xml->DocGetRootElement(xmldoc);
+    XMLNodePointer_t settings = xml->GetChild(root);
+
+    XMLAttrPointer_t attr = xml->GetFirstAttr(settings);
+
+    while(attr!=0)
+    {
+        const char* name = xml->GetAttrName(attr);
+        const char* value = xml->GetAttrValue(attr);
+        std::cout << name << " = " << value << std::endl; 
+        attr = xml->GetNextAttr(attr);
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+// ______________________Regression_________ ___________________________//
+/////////////////////////////////////////////////////////////////////////
+
+void buildAndEvaluateForest()
+{
+// Build a forest with certain parameters and save the trees somewhere.
+
+  ///////////////////////////////////
+  // Train 
+  ///////////////////////////////////
+  
+  // The training and testing events.
+  std::vector<Event*> trainingEvents;
+  std::vector<Event*> testingEvents;
+
+  // Load training events from an ntuple into the training vector.
+  loadEvents("/afs/cern.ch/user/a/acarnes/public/iml/ptrootfiles/Output_Trimmed_97p5_Mode3_100k.root", trainingEvents);
+
+  // Preprocess datasets: apply the transformations and preliminary fits.
+  preprocessTrain(trainingEvents, lf, 0, transform);
+
+  std::cout << std::endl << "Number of training events: " << trainingEvents.size() << std::endl << std::endl;
+
+  // Initialize new forest.
+  Forest* forest = new Forest(trainingEvents);
+
+  // Output the parameters of the current run. 
+  std::cout << "=======================================" << std::endl;
+  std::cout << "Nodes: " << nodes << std::endl;
+  std::cout << "Trees: " << trees << std::endl;
+  std::cout << "Learning Rate: " << lr << std::endl;
+  std::cout << "Loss Function: " << lf->name().c_str() << std::endl;
+
+  if(transform != 0) std::cout << "Transform Function: " << transform->name() << std::endl;
+  else std::cout << "Transform Function: None" << std::endl;
+  std::cout << "=======================================" << std::endl;
+  
+  // Output the save directory to the screen.
+  if(saveTrees) std::cout << "treesDirectory " << treesDirectory.Data() << std::endl;
+
+  // Save the settings for the current regression to XML.
+  // This way the parameters of the forest will be stored as well as the trees.
+  if(saveTrees) saveSettingsToXML(treesDirectory);
+
+  // Do the regression and save the trees.
+  forest->doRegression(nodes, trees, lr, lf, treesDirectory, saveTrees);
+
+  // Rank the variable importance and output it to the screen.
+  std::vector<Int_t> rank;
+  forest->rankVariables(rank);
+
+  // Save the lists of split values for each variable into a file.
+  forest->saveSplitValues("./splitvalues.dat");
+
+  ///////////////////////////////////
+  // Test 
+  ///////////////////////////////////
+
+  // The forest built from the training above is already in memory. There is no need to load from xml.
+  // If you wish to test from a forest that was saved to xml you would do the following.
+  // forest->loadForestFromXML(treesDirectory);
+  // displaySettingsFromXML(treesDirectory); 
+
+  // Read In events.
+  trainingEvents = std::vector<Event*>(); // clear memory of training events
+  loadEvents("/afs/cern.ch/user/a/acarnes/public/iml/ptrootfiles/Output_Trimmed_97p5_TEST_Mode3_100k.root", testingEvents);
+  
+  // Preprocess datasets.
+  preprocessTest(testingEvents, lf, 0, transform);
+  
+  std::cout << "Number of test events: " << testingEvents.size() << std::endl;
+  
+  // Predict the test and rate sets.
+  forest->predictEvents(testingEvents, trees);
+  std::cout << std::endl;
+  
+  // Form the savefile names.
+  TString savetestto = "test_results.root";
+
+  // We want to save the Pt predictions not the transformed Pt predictions that we trained/tested with.
+  invertTransform(testingEvents, transform);
+  std::cout << std::endl;
+  
+  // Save the events. 
+  saveEvents(savetestto, testingEvents);
+  std::cout << std::endl;
+
+  delete forest;
+
+  // ----------------------------------------------------
+  ///////////////////////////////////////////////////////
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// ______________________Main___________________________________________//
+//////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char* argv[])
+{
+// Run a regression with the appropriate settings.
+
+    // Gather regression settings from the command line if you want.
+    // Then you can run as ./TrainAndEvaluate setting1 setting2 ...
+
+    // Simply overwrite the settings at the beginning
+    // with those from the command line like so.
+
+    for(int i=1; i<argc; i++)
+    {
+        std::stringstream ss;
+        ss << argv[i];
+        //if(i==1) ss >> mode;
+    }
+
+    buildAndEvaluateForest();
+    return 0;
+}
---
+++ studies/iml/bin/LoadSaveEvents.h
@@ -0,0 +1,374 @@
+///////////////////////////////////////////////////////////////
+//                  LoadSaveEvents.h                         //
+// ////////////////////////////////////////////////////////////
+//                                                           //
+//  Here we provide loading, saving, pre/post processing     //
+//    functionality.                                         //
+//                                                           //
+///////////////////////////////////////////////////////////////
+
+#ifndef ADD_LOADEVENTS
+#define ADD_LOADEVENTS
+
+#include "Utilities.h"
+#include "Forest.h"
+#include "Functions.h"
+#include "TFile.h"
+#include "TNtuple.h"
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+// ========================================================
+// ================ debug  ================================
+//=========================================================
+
+void listEvents(std::vector<Event*>& events, unsigned int numtolist)
+{
+    for(unsigned int i=0; i<numtolist; i++)
+    {
+        Event* e = events[i];
+        e->outputEvent();
+    }
+}
+
+// ========================================================
+// ================ Preprocess  ===========================
+//=========================================================
+Int_t transformEvents(std::vector<Event*>& events, TransformFunction* transform)
+{
+    if(transform == 0) return 0;
+    //std::cout << "Transforming events ... " << std::endl;
+    Int_t failed = 0;
+
+    for(unsigned int i=0; i<events.size(); i++)
+    {
+        Event* e = events[i];
+        bool failure = false;
+        failure = transform->transform(e); 
+        if(failure)
+        {
+            failed++;
+            //std::cout << "Transforming " << i << " has resulted in an undefined value." << std::endl;
+        }
+    }
+    return failed;
+}
+
+////////////////////////////////////////////////////////////
+//----------------------------------------------------------
+////////////////////////////////////////////////////////////
+
+void preprocessTrain(std::vector<Event*>& events, LossFunction* lf, PreliminaryFit* prelimfit, TransformFunction* transform)
+{
+    std::cout << "Preprocessing training events ... " << std::endl;
+    Int_t failed = 0;
+
+    // Apply the preliminary fit and the transformation for each event.
+    for(unsigned int i=0; i<events.size(); i++)
+    {
+        bool failure = false;
+
+        Event* e = events[i];
+
+        // Apply preliminary fit.
+        if(prelimfit != 0) prelimfit->fit(e);
+        else e->predictedValue = 0;
+
+        // Apply transform to true and predicted values.
+        if(transform != 0) failure = transform->transform(e); 
+
+        // Transforming the truevalue for the event failed.
+        // Having infinite or undefined values for thet truevalue will ruin training,
+        // so we remove these events from the training sample.
+        if(failure)
+        {
+            failed++;
+            std::cout << "Event " << e->id << ": trueValue := TRANFORM(" << e->trueValue << ") " << "is UNDEFINED" << std::endl;
+            std::cout << "Event " << e->id << " has been removed from the collection." << std::endl;
+            events.erase(events.begin()+i);
+            delete e;
+            i--;
+        }
+    }
+
+    // Huber needs the residual quantile and the residual median before assigning the target.
+    // These are set and calculated in the fit function.
+    if(lf->name().compare("Huber")==0) lf->fit(events);
+
+    // Set the initial regression target for each event.
+    for(unsigned int i=0; i<events.size(); i++)
+    {
+       Event* e = events[i];
+       if(prelimfit!=0) e->data[0] = lf->target(e);
+       else e->data[0] = e->trueValue;
+    }
+
+    if(failed > 0)
+        std::cout << "==== NUM REMOVED EVENTS: " << failed << std::endl;
+}
+
+////////////////////////////////////////////////////////////
+//----------------------------------------------------------
+////////////////////////////////////////////////////////////
+
+void preprocessTest(std::vector<Event*>& events, LossFunction* lf, PreliminaryFit* prelimfit, TransformFunction* transform)
+{
+    //std::cout << "Preprocessing test events ... " << std::endl;
+    Int_t failed = 0;
+
+    for(unsigned int i=0; i<events.size(); i++)
+    {
+        bool transformfailure = false;
+
+        Event* e = events[i];
+
+        // Apply preliminary fit.
+        if(prelimfit != 0) prelimfit->fit(e);
+        else e->predictedValue = 0;
+
+        // Apply transform to true and predicted values.
+        if(transform != 0) transformfailure = transform->transform(e); 
+
+        // Transforming the truevalue for this event failed.
+        // This is okay for the testing set and we leave these in.
+        if(transformfailure)
+        {
+            failed++;
+            //std::cout << "Transforming " << e->id << " has resulted in an undefined value." << std::endl;
+        }
+    }
+
+    if(failed > 0)
+        std::cout << "==== NUM UNDEFINED TRANSFORMATIONS: " << failed << std::endl;
+}
+
+////////////////////////////////////////////////////////////
+//----------------------------------------------------------
+////////////////////////////////////////////////////////////
+
+bool preprocessTest(Event* e, LossFunction* lf, PreliminaryFit* prelimfit, TransformFunction* transform)
+{
+    //std::cout << "Preprocessing test events ... " << std::endl;
+
+    bool transformfailure = false;
+
+    // Apply preliminary fit.
+    if(prelimfit != 0) prelimfit->fit(e);
+    else e->predictedValue = 0;
+
+    // Apply transform to true and predicted values.
+    if(transform != 0) transformfailure = transform->transform(e); 
+
+    return transformfailure;
+}
+
+////////////////////////////////////////////////////////////
+//----------------------------------------------------------
+////////////////////////////////////////////////////////////
+
+void preprocessRate(std::vector<Event*>& events, LossFunction* lf, PreliminaryFit* prelimfit, TransformFunction* transform)
+{
+    std::cout << "Preprocessing rate sample ... " << std::endl;
+    Int_t failed = 0;
+
+    for(unsigned int i=0; i<events.size(); i++)
+    {
+        bool transformfailure = false;
+
+        Event* e = events[i];
+       
+        // The rate sample doesn't have a trueValue since it is real data.
+        // Set the trueValue to something that won't screw up the Transformations.
+        e->trueValue = 99999;
+
+        // Apply preliminary fit.
+        if(prelimfit != 0) prelimfit->fit(e);
+        else e->predictedValue = 0;
+
+        // Apply transform to true and predicted values.
+        if(transform != 0) transformfailure = transform->transform(e); 
+
+        // Transforming the truevalue for this event failed.
+        // This is okay for the testing set and we leave these in.
+        if(transformfailure)
+        {
+            failed++;
+            //std::cout << "Transforming " << e->id << " has resulted in an undefined value." << std::endl;
+        }
+    }
+
+    if(failed > 0)
+        std::cout << "==== NUM UNDEFINED TRANSFORMATIONS: " << failed << std::endl;
+}
+
+////////////////////////////////////////////////////////////
+//----------------------------------------------------------
+////////////////////////////////////////////////////////////
+
+bool preprocessRate(Event* e, LossFunction* lf, PreliminaryFit* prelimfit, TransformFunction* transform)
+{
+    //std::cout << "Preprocessing test events ... " << std::endl;
+
+    bool transformfailure = false;
+
+    // The rate sample doesn't have a trueValue since it is real data.
+    // Set the trueValue to something that won't screw up the Transformations.
+    e->trueValue = 99999;
+
+    // Apply preliminary fit.
+    if(prelimfit != 0) prelimfit->fit(e);
+    else e->predictedValue = 0;
+
+    // Apply transform to true and predicted values.
+    if(transform != 0) transformfailure = transform->transform(e); 
+
+    return transformfailure;
+}
+
+// ========================================================
+// ================ Postprocess  ==========================
+//=========================================================
+void invertTransform(std::vector<Event*>& events, TransformFunction* transform)
+{
+    if(transform == 0) return;
+    std::cout << "Untransforming events ... " << std::endl;
+
+    for(unsigned int i=0; i<events.size(); i++)
+    {
+        Event* e = events[i];
+        if(e->trueValue == 0) std::cout << e->id << ": e->trueValue == 0" << std::endl;
+        if(e->predictedValue == 0) std::cout << e->id << ": e->predictedValue == 0" << std::endl;
+        transform->invertTransformation(e); 
+    }
+}
+
+////////////////////////////////////////////////////////////
+//----------------------------------------------------------
+////////////////////////////////////////////////////////////
+
+void invertTransform(Event* e, TransformFunction* transform)
+{
+    if(transform == 0) return;
+    std::cout << "Untransforming event ... " << std::endl;
+
+    if(e->trueValue == 0) std::cout << e->id << ": e->trueValue == 0" << std::endl;
+    if(e->predictedValue == 0) std::cout << e->id << ": e->predictedValue == 0" << std::endl;
+    transform->invertTransformation(e); 
+}
+
+// ========================================================
+// ================ Load Events ===========================
+//=========================================================
+
+//////////////////////////////////////////////////////////////////////////
+// ----------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////
+
+void loadEvents(const char* inputfilename, std::vector<Event*>& events)
+{
+// for the tmva baseline comparison we only work with mode 3 <--> hits in stations 1 and 2 
+    std::cout << "Reading in events from " << inputfilename << " ..." << std::endl;
+
+    // Get the ntuple.
+    TFile* f = new TFile(inputfilename);
+    TNtuple* ntuple = (TNtuple*)f->Get("theNtuple");
+
+    // The variables in the ntuple.
+    Float_t GenPt;
+    Float_t Eta;
+    Float_t dPhi12;
+    Float_t dEta12;
+    Float_t clct1, clct2; 
+
+    // Let the ntuple know about our variables above.
+    ntuple->SetBranchAddress("GenPt", &GenPt);
+    ntuple->SetBranchAddress("Eta", &Eta);
+    ntuple->SetBranchAddress("dPhi12", &dPhi12);
+    ntuple->SetBranchAddress("dEta12", &dEta12);
+    ntuple->SetBranchAddress("clct1", &clct1);
+    ntuple->SetBranchAddress("clct2", &clct2);
+
+    // Loop through the events.
+    for(unsigned int i=0; i<ntuple->GetEntries(); i++)
+    {
+        // Put the info from the ntuple entry into the vars above.
+        ntuple->GetEntry(i);
+
+        // Store the variables needed for prediciton.
+        std::vector<Double_t> x;
+        x.push_back(GenPt);     // target goes in x[0]
+        x.push_back(Eta);       // features go in x[1]->x[N]
+        x.push_back(dPhi12);
+        x.push_back(dEta12);
+        x.push_back(clct1);
+        x.push_back(clct2);     // last feature
+
+        // Load info into the event data structure.
+        Event* e = new Event();
+        e->trueValue = GenPt;
+        e->predictedValue = 0;
+        e->data = x;           // vector with features plus target from above
+        e->id = i;
+
+        // Store in the vector of events.
+        events.push_back(e);
+    }
+    
+    delete ntuple;
+    delete f;
+}
+
+//////////////////////////////////////////////////////////////////////////
+// ______________________Save Events____________________________________//
+//////////////////////////////////////////////////////////////////////////
+
+void saveEvents(const char* savefilename, std::vector<Event*>& events)
+{
+// After using the forest to predict values for a collection of events, save them along with their predicted values into an ntuple.
+
+    std::cout << "Saving events into " << savefilename << "..." << std::endl;
+
+    // Will detail all of the variables used in the regression. They will be saved into the ntuple.
+    TString ntupleVars("GenPt:BDTPt:Eta:dPhi12:dEta12:clct1:clct2");
+
+    // Make a new ntuple.
+    TNtuple* n = new TNtuple("BDTresults", "BDTresults", ntupleVars); 
+
+    // Add events to the ntuple.
+    for(unsigned int i=0; i<events.size(); i++) 
+    {    
+        Event* e = events[i];
+
+        Float_t predictedValue = e->predictedValue;
+        Float_t trueValue = e->trueValue;
+
+        // values to be saved in the ntuple
+        // order defined in ntupleVars string
+        std::vector<Float_t> y;
+
+        // save true and predicted values
+        y.push_back(TMath::Abs(trueValue));
+        y.push_back(TMath::Abs(predictedValue));
+
+        // add feature values
+        for(unsigned int j=1; j<e->data.size(); j++)
+            y.push_back((Float_t) e->data[j]);
+
+        // save into ntuple
+        n->Fill(&y[0]);
+    }
+
+    // Save the ntuple.
+    TFile* f = new TFile(savefilename, "RECREATE");
+    f->cd();
+    n->Write();
+    f->Close();
+    //delete n;
+    delete f;
+}
+
+#endif
+
