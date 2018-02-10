7230d890e9a9c844ed7154621a236d98c93e0a61
Andrew Carnes <carnesandnoble@gmail.com>
2018-02-09 23:08:46 -0500

    Added multithreading.

---
+++ studies/h2mumu/baseline.cxx
@@ -0,0 +1,256 @@
+//////////////////////////////////////////////////////////////////////////
+//                            BasicTrainAndTest.cxx                     //
+// =====================================================================//
+//                                                                      //
+//   Train the forest, save the trees, and test the results.            //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////
+// _______________________Includes_______________________________________//
+///////////////////////////////////////////////////////////////////////////
+
+#include "CategoryReader.h"
+#include "Forest.h"
+#include "SignificanceMetrics.hxx"
+#include "LoadEvents.hxx"
+#include "TRandom3.h"
+#include <sstream>
+#include <fstream>
+#include <map>
+
+//////////////////////////////////////////////////////////////////////////
+// ______________________Categorization_Settings _______________________//
+/////////////////////////////////////////////////////////////////////////
+
+// Set the settings for the regression here. You may choose to overwrite these values in
+// main if you want input from the terminal to determine the settings.
+
+// Fundamental settings for the regression.
+int nbins = 20;
+int trees = 1;
+Int_t nodes = 12;
+double fEvents = 1;
+int nFeatures = 999;
+
+int nbkgmin = 110;
+bool smooth = true;
+TString varset = "bdt_res_eta";
+
+int unctype = 0;
+double nparams = 1;
+bool scale_fluctuations = false;
+bool scale_data = false;
+
+// Whether to save the trees from the regression into a directory specified later.
+bool saveTree = false;
+
+// Where to save the trees.
+TString treeDirectory("./trees/");
+
+TString rootdir("/home/puno/h2mumu/UFDimuAnalysis_v2/bin/rootfiles/bdt/");
+std::vector<TString> rootnames  = {
+//                                   TString("RunB_bdt_training_dyAMC-J.root"),
+//                                   TString("RunC_bdt_training_dyAMC-J.root"),
+//                                   TString("RunD_bdt_training_dyAMC-J.root"),
+//                                   TString("RunE_bdt_training_dyAMC-J.root"),
+//                                   TString("RunF_1_bdt_training_dyAMC-J.root"),
+//                                   TString("RunF_2_bdt_training_dyAMC-J.root"),
+//                                   TString("RunG_bdt_training_dyAMC-J.root"),
+//                                   TString("RunH_bdt_training_dyAMC-J.root"),
+                                   TString("H2Mu_VBF_bdt_training_dyAMC-J.root"),
+                                   TString("H2Mu_gg_bdt_training_dyAMC-J.root"),
+//                                   TString("H2Mu_WH_neg_bdt_training_dyAMC-J.root"),
+//                                   TString("H2Mu_WH_pos_bdt_training_dyAMC-J.root"),
+//                                   TString("H2Mu_ZH_bdt_training_dyAMC-J.root"),
+                                   TString("ZJets_AMC_0j_bdt_training_dyAMC-J.root"),
+                                   TString("ZJets_AMC_1j_bdt_training_dyAMC-J.root"),
+                                   TString("ZJets_AMC_2j_bdt_training_dyAMC-J.root"),
+                                   TString("tt_ll_AMC_bdt_training_dyAMC-J.root")
+//                                   TString("WW_bdt_training_dyAMC-J.root"),
+//                                   TString("WZ_3l_bdt_training_dyAMC-J.root"),
+//                                   TString("ZZ_4l_bdt_training_dyAMC-J.root"),
+//                                   TString("ZZ_2l_2q_bdt_training_dyAMC-J.root"),
+//                                   TString("ZZ_2l_2v_bdt_training_dyAMC-J.root"),
+//                                   TString("tW_neg_bdt_training_dyAMC-J.root"),
+//                                   TString("tW_pos_bdt_training_dyAMC-J.root"),
+//                                   TString("tZq_bdt_training_dyAMC-J.root"),
+//                                   TString("ttW_bdt_training_dyAMC-J.root"),
+//                                   TString("ttZ_bdt_training_dyAMC-J.root")
+                                   };
+
+std::vector<std::string> useWhichVars;
+
+//////////////////////////////////////////////////////////////////////////
+//______________________Features_to_Use________________________________//
+/////////////////////////////////////////////////////////////////////////
+
+void initWhichVars(std::vector<std::string>& useWhichVars)
+{
+    ////////////////////////////////////////////
+    // bdt score
+    if(varset.Contains("bdt")) 
+        useWhichVars.push_back("bdt_score");               
+
+    ////////////////////////////////////////////
+    // resolution variables
+
+    // resolution variables 
+    if(varset.Contains("res_eta")) 
+    {
+        //useWhichVars.push_back("dimu_avg_abs_eta");               
+        //useWhichVars.push_back("dimu_min_abs_eta");               
+        useWhichVars.push_back("dimu_max_abs_eta");               
+    }
+    if(varset.Contains("res_mass")) 
+    {
+        useWhichVars.push_back("massErr_Roch");               
+    }
+
+    ////////////////////////////////////////////
+    // run2 bdt training vars + resolution var
+    if(varset.Contains("run2"))
+    {
+        useWhichVars.push_back("dimu_max_abs_eta");               
+        useWhichVars.push_back("dimu_pt");
+        useWhichVars.push_back("dimu_eta");
+        useWhichVars.push_back("dimu_abs_dEta");
+        useWhichVars.push_back("dimu_abs_dPhi");
+        useWhichVars.push_back("jet1_pt");
+        useWhichVars.push_back("jet1_eta");
+        useWhichVars.push_back("jet2_pt");
+        useWhichVars.push_back("jet2_eta");
+        useWhichVars.push_back("dijet1_mass");
+        useWhichVars.push_back("dijet1_abs_dEta");
+        useWhichVars.push_back("dijet2_mass");
+        useWhichVars.push_back("dijet2_abs_dEta");
+        useWhichVars.push_back("nJets");
+        useWhichVars.push_back("nJetsCent");          
+        useWhichVars.push_back("nJetsFwd");          
+        useWhichVars.push_back("nBMed");
+        useWhichVars.push_back("MET");
+    }
+    
+}
+
+void listEvents(std::vector<Event*>& events, unsigned int N)
+{
+    for(unsigned int i=0; i<events.size(); i++)
+    {
+        if(i>=N) return;
+        std::cout << events[i]->id << " [" << events[i]->trueValue << "]: " << events[i]->predictedValue << std::endl;
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+// ______________________Regression_________ ___________________________//
+/////////////////////////////////////////////////////////////////////////
+
+void buildCategorization()
+{
+// Build a tree and save it in xml
+
+  ///////////////////////////////////
+  // Train 
+  ///////////////////////////////////
+
+  // Choose which significance function to use.
+  SignificanceMetric* sf = new PoissonSignificance(unctype, nparams, nbkgmin, scale_fluctuations, scale_data, smooth);
+  //SignificanceMetric* sf = new AsimovSignificance(unctype, nbkgmin, scale);
+
+  // The training and testing events.
+  std::vector<Event*> trainingEvents = std::vector<Event*>();
+
+  initWhichVars(useWhichVars);
+  //for(auto& filename: csvnames)
+  //    loadEventsCSV(trainingEvents, useWhichVars, csvdir+filename);
+
+  for(auto& filename: rootnames)
+      loadEventsROOT(trainingEvents, useWhichVars, rootdir+filename);
+
+  std::cout << std::endl << "Number of training events: " << trainingEvents.size() << std::endl << std::endl;
+
+  // Calculate the number of features to use in each tree of the random forest
+  //int nFeatures = (int) TMath::Sqrt(useWhichVars.size());
+  if (nFeatures < 1) nFeatures = 1;
+
+  // Initialize new forest.
+  Forest forest(trainingEvents, fEvents, nFeatures);
+  forest.setFeatureNames(useWhichVars);
+  
+  // Output the parameters of the current run. 
+  std::cout << "=========================================" << std::endl;
+  std::cout << "varset             : " << varset << std::endl;
+  std::cout << "Trees              : " << trees << std::endl;
+  std::cout << "Nodes              : " << nodes << std::endl;
+  std::cout << "fEvents            : " << fEvents << std::endl;
+  std::cout << "nFeatures          : " << nFeatures << std::endl;
+  std::cout << "N_bkg_min          : " << nbkgmin << std::endl;
+  std::cout << "smooth             : " << smooth << std::endl;
+  std::cout << "unctype            : " << unctype << std::endl;
+  std::cout << "nparams            : " << nparams << std::endl;
+  std::cout << "scale_fluctuations : " << scale_fluctuations << std::endl;
+  std::cout << "scale_data         : " << scale_data << std::endl;
+  std::cout << "Significance Metric: " << sf->name << std::endl;
+  std::cout << "tree save dir      : "  << treeDirectory << std::endl;
+  std::cout << "=========================================" << std::endl;
+
+  // Do the regression and save the trees.
+  forest.doRegression(nodes, trees, nbins, sf, treeDirectory.Data(), saveTree);
+
+  // Rank the variable importance and output it to the screen.
+  forest.outputFeatureRankings();
+
+  // ----------------------------------------------------
+  ///////////////////////////////////////////////////////
+
+  //XMLCategorizer xmlc(treeDirectory+savename);
+  //xmlc.outputCategories();
+
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// ______________________Main___________________________________________//
+//////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char* argv[])
+{
+// Run a regression with the appropriate settings.
+
+    // Gather regression settings from the command line if you want.
+    // Then you can run as ./BasicTrainAndTest setting1 setting2 ...
+
+    // Simply overwrite the settings at the beginning
+    // with those from the command line like so.
+    // Otherwise it will use the default settings listed at the top of this file
+
+    for(int i=1; i<argc; i++)
+    {
+        std::stringstream ss;
+        ss << argv[i];
+        if(i==1) varset = ss.str().c_str(); // string telling which variables to use for categorization
+        if(i==2) ss >> trees;               // the number of categories 
+        if(i==3) ss >> nodes;               // the number of categories 
+        if(i==4) ss >> fEvents;             // the number of categories 
+        if(i==5) ss >> nFeatures;           // the number of categories 
+        if(i==6) ss >> nbkgmin;             // the smallest amount of background allowed in a bin (prevent overtraining)
+        if(i==7) ss >> smooth;              // smooth the estimate of the bkg in a bin by averaging it with its neighboring bins
+        if(i==8) ss >> unctype;             // the uncertainty type to use
+        if(i==9) ss >> scale_fluctuations;  // scale the fluctuations using an adhoc estimate based upon the bkg out of the window
+        if(i==10) ss >> scale_data;         // scale the bkg in the window based upon ndata/nbkg outside the window
+        if(i==11) ss >> nparams;            // this is only important for a certain uncertainty type
+    }
+
+    // of course set the varset string to whatever you want, and the nodes to whatever (15 is good)
+    // Other than those options, I recommend using nbkgmin=25, unctype=0 (no extra uncertainty), and smooth=1 
+    // the rest of the options can be set to 0
+    
+    // If the bkg MC and the data don't match then you can turn on scale_data
+    // scale_fluctuations is better left off, smooth is a better way to curtail downward bkg fluctuations
+
+    // Use AsimovSignificance as a metric instead of Poisson if you want, but it didn't make a big difference for me
+
+    buildCategorization();
+    return 0;
+}
